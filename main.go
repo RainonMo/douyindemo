0ebae7168a6cfbb34d140b38f2f804635ae3992e
momo <mochunt@163.com>
2022-06-13 14:50:51 +0800

    ok

--- main.go
+++ main.go
@@ -12,11 +12,6 @@ func main() {
 	
 	engine := gin.Default()
 
-	// 开启跨域
-	//engine.Use(middleware.Cors())
-
-	// 静态资源请求映射
-	// engine.Static("/video", global.Config.Upload.SavePath)
 	engine.Static("/static", "./public")
 
 	// 后台管理员前端接口
@@ -35,11 +30,6 @@ func main() {
 		 web.GET("/user/", controller.UserInfo)
 		 web.POST("/user/register/", controller.Register)
 
-		web.GET("/feed/", controller.Feed)
-		//web.POST("/publish/action/", controller.Publish)
-
-		// 开启JWT认证，以下接口需要认证成功才能访问
-		//web.Use(middleware.JwtAuth())
 
 	}
 
